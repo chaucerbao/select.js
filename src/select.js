dd8d17bac5e3dadafafc326e20d9dfa6f55ecc28
Alvin Chan <aschan@gmail.com>
2016-04-06 18:41:14 -0700

    Refactor functions to use a global "activeSelect" variable
    Rather than continually passing a "select" variable through functions,
    use a globally available "activeSelect" variable to reference the
    currently open dropdown.

--- src/select.js
+++ src/select.js
@@ -11,12 +11,12 @@
   var KEYDOWN = "keydown";
   var DATA_INDEX = "data-index";
 
+  var shouldDetachOptions = false;
   var activeSelect;
 
   // Shortcuts
   function $(selector, context) { return (context || document).querySelector(selector); }
   function $$(selector, context) { return (context || document).querySelectorAll(selector); }
-  function isOpen(select) { return select.classList.contains(SELECT_FOCUSED); }
   function addClass(element, classes) { element.classList.add(classes); }
   function removeClass(element, classes) { element.classList.remove(classes); }
   function appendChild(element, child) { element.appendChild(child); }
@@ -41,8 +41,10 @@
       var option = options[i];
       var optionElement = createElement("li");
 
+      // Link the list item to its <option> tag by using the same DATA_INDEX
       option.setAttribute(DATA_INDEX, i);
       optionElement.setAttribute(DATA_INDEX, i);
+
       addClass(optionElement, OPTION);
       appendChild(optionElement, document.createTextNode(option.textContent));
 
@@ -79,31 +81,24 @@
 
     // Close the option list when the overlay is clicked
     addEventListener($("." + OVERLAY, select), MOUSEDOWN, function(e) {
-      e.stopPropagation();
-
-      closeSelectBox(select, true);
+      closeSelectBox(true);
     });
 
     // Select an option when an option list item is clicked
     addEventListener($("." + OPTIONS, select), MOUSEDOWN, function(e) {
-      var index = optionIndex(e.target);
-
-      selectOption(select, index);
-      closeSelectBox(select);
+      selectOption(optionIndex(e.target));
+      closeSelectBox();
     });
 
     // Keyboard controls
-    addEventListener(select, KEYUP, function() {
-      selectOption(select, optionIndex($(OPTION_CHECKED, select)));
-    });
     addEventListener(select, KEYDOWN, function(e) {
-      var selected = $(OPTION_CHECKED, select);
+      var selected = $(OPTION_CHECKED, activeSelect ? activeSelect.optionTags : select);
 
       switch (e.keyCode) {
         case 9:
           // Tab
           if (activeSelect) {
-            closeSelectBox(select);
+            closeSelectBox();
           }
           break;
         case 13:
@@ -113,14 +108,14 @@
           break;
         case 27:
           // ESC
-          closeSelectBox(select, true);
+          closeSelectBox(true);
           break;
         case 38:
         case 40:
           // (Up/Down)-arrow
           var toOption;
 
-          if (!isOpen(select)) {
+          if (!activeSelect) {
             // Open
             openSelectBox(select);
           } else if (e.keyCode === 38) {
@@ -132,7 +127,7 @@
           }
 
           if (toOption) {
-            selectOption(select, optionIndex(toOption));
+            selectOption(optionIndex(toOption));
           }
           break;
       }
@@ -140,26 +135,52 @@
   }
 
   function openSelectBox(select) {
-    if (!isOpen(select)) {
-      activeSelect = {
-        initialIndex: optionIndex($(OPTION_CHECKED, select)),
-        optionList: $("." + OPTIONS, select)
-      };
-      selectOption(select, activeSelect.initialIndex);
+    if (activeSelect) { return; }
 
-      // Reveal the option list
-      addClass(select, SELECT_FOCUSED);
+    var optionTags;
 
-      reqAnimationFrame(renderOptionList);
+    if (shouldDetachOptions) {
+      // Detach <option> tags from the <select>, for browsers that insist on showing the native option list
+      var selectElement = $("select", select);
+
+      optionTags = document.createDocumentFragment();
+
+      while (selectElement.firstElementChild) {
+        appendChild(optionTags, selectElement.firstElementChild);
+      }
+    } else {
+      optionTags = $("select", select);
     }
+
+    activeSelect = {
+      element: select,
+      initialIndex: optionIndex($(OPTION_CHECKED, optionTags)),
+      optionTags: optionTags
+    };
+
+    selectOption(activeSelect.initialIndex);
+
+    // Reveal the option list
+    addClass(select, SELECT_FOCUSED);
+
+    reqAnimationFrame(renderOptionList);
   }
 
-  function closeSelectBox(select, shouldEscape) {
+  function closeSelectBox(shouldEscape) {
+    if (!activeSelect) { return; }
+
+    var select = activeSelect.element;
+
     removeClass(select, SELECT_FOCUSED);
 
     // Reset to the previously selected option
-    if (shouldEscape && activeSelect) {
-      selectOption(select, activeSelect.initialIndex);
+    if (shouldEscape) {
+      selectOption(activeSelect.initialIndex);
+    }
+
+    // Reattach <option> tags to the <select>
+    if (shouldDetachOptions) {
+      $("select", select).appendChild(activeSelect.optionTags);
     }
 
     // Trigger the "change" event if the selected option has changed
@@ -171,54 +192,60 @@
   }
 
   function toggleSelectBox(select) {
-    if (isOpen(select)) {
-      closeSelectBox(select);
+    if (activeSelect) {
+      closeSelectBox();
     } else {
       openSelectBox(select);
     }
   }
 
-  function selectOption(select, toIndex) {
+  function selectOption(toIndex) {
+    var select = activeSelect.element;
     var attributeIdentifier = "[" + DATA_INDEX + "=\"" + toIndex + "\"]";
 
     // Mark the <option> tag as selected
-    $("option" + attributeIdentifier, select).selected = true;
+    var optionTags = activeSelect.optionTags;
+    var selectedOption = $(OPTION_CHECKED, optionTags);
+    if (selectedOption) {
+      selectedOption.selected = false;
+    }
+    $("option" + attributeIdentifier, optionTags).selected = true;
 
-    // Update the option list
+    // Update the selected item in the option list
     removeClass($("." + OPTION_SELECTED, select), OPTION_SELECTED);
     addClass($("." + OPTION + attributeIdentifier, select), OPTION_SELECTED);
   }
 
   function renderOptionList() {
-    if (activeSelect) {
-      var optionList = activeSelect.optionList ;
-      var optionListRect = optionList.getBoundingClientRect();
-      var selectRect = optionList.parentNode.getBoundingClientRect();
+    if (!activeSelect) { return; }
 
-      if (optionList.classList.contains(OPTIONS_TOP)) {
-        // Option list is above the custom-select
-        if (
-          // Option list is past the top of the window
-          optionListRect.top < 0 ||
-          // Or, option list can fit below the custom-select
-          selectRect.bottom + optionListRect.height < window.innerHeight
-        ) {
-          removeClass(optionList, OPTIONS_TOP);
-        }
-      } else {
-        // Option list is below the custom-select
-        if (
-          // Option list is past the bottom of the window
-          optionListRect.top + optionListRect.height > window.innerHeight &&
-          // And, option list can fit above the custom-select
-          selectRect.top - optionListRect.height > 0
-        ) {
-          addClass(optionList, OPTIONS_TOP);
-        }
+    var optionList = $("." + OPTIONS, activeSelect.element);
+    var optionListRect = optionList.getBoundingClientRect();
+    var selectRect = optionList.parentNode.getBoundingClientRect();
+
+    if (optionList.classList.contains(OPTIONS_TOP)) {
+      // Option list is above the custom-select
+      if (
+        // Option list is past the top of the window
+        optionListRect.top < 0 ||
+        // Or, option list can fit below the custom-select
+        selectRect.bottom + optionListRect.height < window.innerHeight
+      ) {
+        removeClass(optionList, OPTIONS_TOP);
       }
-
-      reqAnimationFrame(renderOptionList);
+    } else {
+      // Option list is below the custom-select
+      if (
+        // Option list is past the bottom of the window
+        optionListRect.top + optionListRect.height > window.innerHeight &&
+        // And, option list can fit above the custom-select
+        selectRect.top - optionListRect.height > 0
+      ) {
+        addClass(optionList, OPTIONS_TOP);
+      }
     }
+
+    reqAnimationFrame(renderOptionList);
   }
 
   function initialize(shouldOpen, e) {
